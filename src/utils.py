cc78f0710a1f1f51c4a09eb1038836fc84c8ce65
spawner <spawner6@outlook.com>
2025-05-15 19:44:41 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -14,9 +14,9 @@ def setup_logger(log_dir, run_name, log_level=logging.INFO):
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     
-    file_handler = logging.FileHandler(os.path.join(log_dir, f"{run_name}.log"), mode='a', encoding='utf-8')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    #file_handler = logging.FileHandler(os.path.join(log_dir, f"{run_name}.log"), mode='a', encoding='utf-8')
+    #file_handler.setFormatter(formatter)
+    #logger.addHandler(file_handler)
     
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
